62535add98c9b89a473b928c1830f388f3a74078
Zachary Bluedorn <zachary.bluedorn@gmail.com>
2016-11-14 17:40:30 -0800

    change routing and file structure for lessons

--- routes/lessons.js
+++ routes/lessons.js
@@ -2,22 +2,45 @@
 
 let express = require('express');
 let router = express.Router();
-//let fs = require('fs')
+let fs = require('fs')
 //let lessons = JSON.parse(fs.readFileSync('routes/lessons.json', 'utf8'))
 
-
-/* GET users listing. */
 router.get('/', (req, res, next) => {
   res.render('lessons', {title: "Lessons"})
 })
 
-router.get('/:lesson', (req, res, next) => {
+router.get('/:lesson/next', (req, res, next) => {
+  // open next lesson based on current page
+  //
+  // pass "next" and "previous" URLs to page as parameters for the links
+  // if next/previous don't exist, send "null" and put logic in view to gray out if null
+  //
+  //
   //let lesson = lessons.filter(lesson => lesson.name === req.params.lesson)[0]
-  let lessonPath = 'lessons/' + req.params.lesson
-
-  res.render(lessonPath)
 })
 
+router.get('/:lesson/:page', (req, res, next) => {
+  let lesson = req.params.lesson
+  let page   = req.params.page
+  let path   = lesson + '/' + page
 
+  // Previous page for "Previous" link
+  let previousPageNumber = page.parseInt() - 1
+  let previousPagePath = lesson + '/' + previousPageNumber
+  try {
+    fs.accessSync(previousPagePath)
+    let previous = previousPagePath
+  } catch(e) let previous = "None"
+
+  // Next page for "Next" link
+  let nextPageNumber = page.parseInt() + 1
+  let nextPagePath = lesson + '/' + nextPageNumber
+  try {
+    fs.accessSync(nextPagePath)
+    let next = nextPagePath
+  } catch(e) let next = "None"
+
+  res.render(path, { previous: previous, next: next })
+})
 
 module.exports = router;
